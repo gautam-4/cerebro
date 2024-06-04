ec85afc911bcbbc579430afd866ce3714f8198f3
gautam-4 <gautamalt468@gmail.com>
2024-06-04 23:22:14 +0530

    add branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,14 +9,9 @@ import LandingPage from './LandingPage';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-        <Route path='/' element={<><Header isLoggedIn = {false}/><LandingPage/><Register/></>}></Route>
-          <Route path='/login' element={<><Header isLoggedIn = {false}/><Login /></>}></Route>
-          <Route path='/register' element={<><Header isLoggedIn = {false}/><Register/></>}></Route>
-          <Route path='/home' element={<><Header isLoggedIn = {true}/><MainContent /></>}></Route>
-        </Routes>
-      </BrowserRouter>
+
+          <><Header isLoggedIn = {true}/><MainContent /></>
+    
 
     </>
   )
